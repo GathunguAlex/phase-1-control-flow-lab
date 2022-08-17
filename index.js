4cc2b23b5c88e101c20e177b3a7bd99b787414cf
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-08-17 21:57:45 +0300

    commit tree e38160bf0f58aa46e710d0cefce46afd9ea6c880

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let tip;
+//let feet;
+//let city;
+function scuberGreetingForFeet(feet){
+  let repo;
+  if (feet<=400){
+    repo =`This one is on me!`
+  }
+  else if(feet>= 2001 && feet < 2500){
+    repo = `I will gladly take your thirty bucks.`
+  }
+  else if(feet>=2500){
+    repo = `No can do.`
+  }
+  return repo;
 }
+//scuberGreetingForFeet(199);
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let response
+  city === 'NYC' ? 
+     (response = `Ok, sounds good.`) : (response ='No go.');
+     return response;
 }
+ternaryCheckCity('Pitsburgh');
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let charm;
+  switch(tip){
+    case 'generous':
+    charm = 'Thank you so much.';
+   break;
+   case 'not as generous':
+    charm = 'Thank you.';
+   break;
+  default :
+    charm = 'Bye.';
+    break;
+  }
+  return charm;
 }
+switchOnCharmFromTip('not as generous');
